e494e79b537332e5596086bbd9b99fb335b22c7d
Nageshwar <nageshpawarpatil@gmail.com>
2024-06-22 22:04:23 +0530

    Day-01-React-I-[Intro-To-React] Revision Done

--- Cap-06/Module-02-React/Day-01-React-I-[Intro-To-React]/Assignments/Flex-Box/script.js
+++ Cap-06/Module-02-React/Day-01-React-I-[Intro-To-React]/Assignments/Flex-Box/script.js
@@ -1,21 +1,30 @@
-let element = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", {
-        children: 1,
-        className: "child",
+let element = React.createElement("div", {
+  id: "parent",
+  className: "parent",
+  children: [
+    ,
+    React.createElement("p", {
+      children: "Parent",
+      className: "child",
     }),
     React.createElement("div", {
-        children: 2,
-        className: "child",
+      children: 1,
+      className: "child",
     }),
     React.createElement("div", {
-        children: 3,
-        className: "child",
+      children: 2,
+      className: "child",
     }),
     React.createElement("div", {
-        children: 4,
-        className: "child",
+      children: 3,
+      className: "child",
     }),
-]);
+    React.createElement("div", {
+      children: 4,
+      className: "child",
+    }),
+  ],
+});
 
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
 rootElement.render(element);
