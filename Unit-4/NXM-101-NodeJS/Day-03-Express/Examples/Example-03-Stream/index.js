b4262376f0bd212824236c5b1117e5a30b9d3adc
Nageshwar <nageshpawarpatil@gmail.com>
2024-07-14 23:24:37 +0530

    Stream Added

--- Unit-4/NXM-101-NodeJS/Day-03-Express/Examples/Example-03-Stream/index.js
+++ Unit-4/NXM-101-NodeJS/Day-03-Express/Examples/Example-03-Stream/index.js
@@ -10,8 +10,25 @@ app.get("/", (req, res) => {
 });
 
 app.get("/movie", (req, res) => {
-    const movie_chunks = fs.createReadStream("./data.txt", "utf-8");
-    movie_chunks.pipe(res);
+  const movie_chunks = fs.createReadStream("./data.txt", "utf-8");
+  movie_chunks.pipe(res);
+});
+app.post("/addMovie", (req, res) => {
+  const writeStream = fs.createWriteStream("./data.txt", {
+    flags: "a",
+  });
+
+  writeStream.write(
+    `Title: ${req.body.title}, Year: ${req.body.year}\n`,
+    (err) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send("Error writing to file");
+      }
+      res.send("Movie added successfully");
+    }
+  );
+  writeStream.end();
 });
 
 app.listen(8080, () => {
