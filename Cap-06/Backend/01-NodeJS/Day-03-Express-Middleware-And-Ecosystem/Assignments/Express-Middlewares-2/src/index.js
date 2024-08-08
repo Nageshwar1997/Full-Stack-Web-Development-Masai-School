679c239e85c0d002b801bc0ac3e9ef0eba691804
Nageshwar <nageshpawarpatil@gmail.com>
2024-08-08 16:58:44 +0530

    Express-Middlewares-2 Assignment Done

---
+++ Cap-06/Backend/01-NodeJS/Day-03-Express-Middleware-And-Ecosystem/Assignments/Express-Middlewares-2/src/index.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const express = require("express");
+const multer = require("multer");
+const cloudinary = require("cloudinary");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const uploadDir = path.join(__dirname, "uploads");
+
+// Ensure the uploads directory exists
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET_KEY,
+});
+
+// Configure Multer for file uploads
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now();
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/upload", upload.single("avatar"), async (req, res) => {
+  try {
+    const uploadResult = await cloudinary.uploader.upload(req.file.path);
+
+    fs.unlinkSync(req.file.path);
+
+    res.status(200).json({
+      message: "File uploaded successfully",
+      imageUrl: uploadResult.url,
+    });
+  } catch (error) {
+    res.status(500).send("Error uploading file");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
